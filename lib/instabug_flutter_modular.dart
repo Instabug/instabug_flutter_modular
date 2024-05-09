8603540b40b0a4873d6b2de7789a1f6084f214b2
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2024-05-09 12:02:49 +0300

    feat: support wrapping modular routes for screen loading (#1)
    * feat: support wrapping modular routes for screen loading
    
    * feat: expose wrap routes for wrapping routes without wrapping modules

--- lib/instabug_flutter_modular.dart
+++ lib/instabug_flutter_modular.dart
@@ -1,7 +1,2 @@
-library instabug_flutter_modular;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/instabug_module.dart';
+export 'src/instabug_modular.dart';
