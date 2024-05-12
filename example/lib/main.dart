96303dd74d986aa73957df02e6692a7fcaf53673
Andrew Amin <160974398+AndrewAminInstabug@users.noreply.github.com>
2024-05-12 17:31:43 +0300

    feat: add example app to flutter modular V5

---
+++ example/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_demo_app/modules.dart';
+import 'package:instabug_flutter/instabug_flutter.dart';
+import 'package:instabug_flutter_modular/instabug_flutter_modular.dart';
+
+void main() {
+  runApp(
+    ModularApp(
+      module: InstabugModule(AppModule()),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+
+  @override
+  void initState() {
+    super.initState();
+    Instabug.init(
+      token: 'ed6f659591566da19b67857e1b9d40ab',
+      invocationEvents: [InvocationEvent.floatingButton],
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+      title: 'Flutter Modular Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
---
+++ example/lib/screens/home_page.dart
@@ -0,0 +1,100 @@
+part of '../screens.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _inatsbug = true, _apm = true, _screenLoading = true, _uiTrace = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CustomSwitchListTile(
+              value: _inatsbug,
+              onChanged: (value) {
+                Instabug.setEnabled(value);
+                setState(() => _inatsbug = value);
+              },
+              title: "Instabug",
+            ),
+            CustomSwitchListTile(
+              value: _apm,
+              onChanged: (value) {
+                APM.setEnabled(value);
+                setState(() => _apm = value);
+              },
+              title: "APM",
+            ),
+            CustomSwitchListTile(
+              value: _screenLoading,
+              onChanged: (value) {
+                APM.setScreenLoadingEnabled(value);
+                setState(() => _screenLoading = value);
+              },
+              title: "Screen Loading",
+            ),
+            CustomSwitchListTile(
+              value: _uiTrace,
+              onChanged: (value) {
+                APM.setAutoUITraceEnabled(value);
+                setState(() => _uiTrace = value);
+              },
+              title: "UI Trace",
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/simplePage'),
+              title: 'Navigate To Simple Page',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.pushNamed('/complexPage'),
+              title: 'Navigate To Complex Page',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.pushNamed('/simplePage'),
+              title: 'Navigate To Second Page (using push)',
+            ),
+            CustomButton(
+              onPressed: () =>
+                  Modular.to.navigate('/simplePage?name=Inastabug'),
+              title: 'Navigate To Second Page (with params)',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/redirect'),
+              title: 'Navigate Using RedirectRoute',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/bindsPage'),
+              title: 'Navigate To Binds Page',
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/secondModule/'),
+              title: 'Navigate To Second Module (Nested Routes)',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/thirdModule/'),
+              title: 'Navigate To Third Module (Binds)',
+            ),
+            CustomButton(
+              onPressed: () => Modular.to.navigate('/asde'),
+              title: 'Navigation History',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
